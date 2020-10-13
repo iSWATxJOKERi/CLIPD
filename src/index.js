d30af1a06123eea8c22f3dddc1ceea0e18fb9158
swatjoker <iSWATxJOKERi>
2020-10-13 09:50:13 -0400

    able to get data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
 import '../src/styles/index.scss';
+import { getMatch, getPlayerByName, getTwitchUser, getTelemetry, getVideos, getPubgVideos, timeGreaterThan, timeGreaterThan2, timestamp } from './scripts/search_utilities';
+import "regenerator-runtime/runtime";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let BLACKLISTED = {};
+    let gamertag = document.getElementsByClassName("gamertag-field")[0].value;
+    document.querySelector(".fa-search").addEventListener("click", getPlayer);
+
+    let actualMatches = [];
+    let events = [];
+    let telemetryEvents = [];
+    let clips = [];
+    async function getPlayer() {
+        let matches = await getPlayerByName(gamertag);
+        actualMatches = matches.map(async match => {
+            return await getMatch(match.id)
+        })
+
+        let games = await Promise.allSettled(actualMatches);
+        // console.log(games)
+        games.forEach(async match => {
+            if(match.value) {
+                if(match.value.included) {
+                    match.value.included.forEach(async ele => {
+                        if(ele.id === match.value.data.relationships.assets.data[0].id) {
+                            events.push(getTelemetry(ele.attributes.URL))
+                        }
+                    })
+                }
+            }
+        })
+
+        let telemetry = await Promise.allSettled(events);
+        // console.log(telemetry)
+        telemetry.forEach(event => {
+            event.value.forEach(log => {
+                if(((log._T === "LogPlayerKill" && log.killer) && log.killer.name === gamertag) || ((log._T === "LogPlayerKill" && log.victim) && log.victim.name === gamertag)){
+                    telemetryEvents.push(log)
+                }
+            })
+        })
+        // console.log(telemetryEvents)
+        for(const tEvent of telemetryEvents) {
+            let eventTimestamp = tEvent._D;
+            if(tEvent.killer) {
+                if(!BLACKLISTED[tEvent.killer.name]){
+                    let twitchUser = await getTwitchUser(tEvent.killer.name).then(function(response) {
+                        if(response.ok) {
+                            return response.json().then(json => {
+                                return json
+                            })
+                        } else {
+                            return false
+                        }
+                    })
+                    if(twitchUser) {
+                        if(twitchUser.data.length > 0) {
+                            let videos = await getVideos(twitchUser.data[0].id);
+                            if(videos.data.length > 0) {
+                                videos.data.map(async vid => {
+                                    let clip = await getPubgVideos(vid.id).then(function(response) {
+                                        if(response.ok) {
+                                            return response.json()
+                                        }
+                                    })
+                                    if(clip.data) {
+                                        if(clip.data.game === "PLAYERUNKNOWN'S BATTLEGROUNDS") {
+                                            if(timeGreaterThan(eventTimestamp, clip.data.created_at) && timeGreaterThan2(eventTimestamp, clip.data.created_at, clip.data.length)) {
+
+                                                clips.push({"url": clip.data.url, "timestampInSeconds": timestamp(eventTimestamp, clip.data.created_at, clip.data.length), "event": tEvent, "vod": clip.data})
+                                            }
+                                        }
+                                    }
+                                })
+                            }
+                            BLACKLISTED[tEvent.killer.name] = tEvent.killer.name;
+                        }
+                    } else {
+                        BLACKLISTED[tEvent.killer.name] = tEvent.killer.name;
+                    }
+                }
+            }
+        }
+        let final = await Promise.allSettled(clips);
+        console.log(final);
+    }
+})
---
+++ src/scripts/search_utilities.js
@@ -0,0 +1,166 @@
+import Key from '../config/keys';
+
+export const getPlayerByName = gamertag => {
+    const playerByNameInit = {
+        method: 'get',
+        headers: {
+            Authorization: `Bearer ${ Key.pubgAPI }`,
+            Accept: 'application/vnd.api+json'
+        }
+    }
+    let request = new Request(`https://api.pubg.com/shards/xbox/players?filter[playerNames]=${ gamertag }`, playerByNameInit);
+    return fetch(request).then(function(response) {
+        if(response.ok) {
+            // debugger
+            return response.json().then(json => {
+                return json.data[0].relationships.matches.data;
+            })
+        }
+    })
+}
+window.getPlayerByName = getPlayerByName;
+
+export const getMatch = (matchId) => {
+    
+    const gameInit = {
+        method: 'get',
+        headers: {
+            Accept: 'application/vnd.api+json'
+        }
+    }
+
+    let request = new Request(`https://api.pubg.com/shards/xbox/matches/${ matchId }`, gameInit);
+    return fetch(request).then(function(response) {
+        if(response.ok) {
+            return response.json().then(match => {
+                return match
+            })
+        }
+    })
+}
+window.getMatch = getMatch;
+
+export const getTelemetry = (url) => {
+    const telemetryInit = {
+        method: 'get',
+        headers: {
+            Accept: 'application/vnd.api+json'
+        }
+    }
+
+    let request = new Request(url, telemetryInit);
+    return fetch(request).then(function(response) {
+        if(response.ok) {
+            return response.json().then(json => {
+                return json
+            })
+        }
+    })
+}
+window.getTelemetry = getTelemetry;
+
+export const getOAuth = () => {
+    const oauthInit = {
+        method: 'post',
+        // scope: 'user:read:email'
+
+    }
+    let request = new Request(`https://id.twitch.tv/oauth2/token?client_id=${ Key.twitchAPI }&client_secret=${ Key.clientSECRET }&grant_type=client_credentials`, oauthInit);
+    return fetch(request).then(function(response) {
+        if(response.ok) {
+            return response.json()
+        }
+    })
+}
+
+export const getTwitchUser = gamertag => {
+    const twitchUserInit = {
+        method: 'get',
+        headers: {
+            'Authorization': `Bearer ${ Key.oAUTH }`,
+            'Client-Id': `${ Key.twitchAPI }`
+        }
+    }
+    let request = new Request(`https://api.twitch.tv/helix/users?login=${ gamertag }`, twitchUserInit);
+    return fetch(request)
+}
+window.getTwitchUser = getTwitchUser;
+
+export const getVideos = userId => {
+    const twitchVideosInit = {
+        method: 'get',
+        headers: {
+            'Authorization': `Bearer ${ Key.oAUTH }`,
+            'Client-Id': `${ Key.twitchAPI }`
+        }
+    }
+    let request = new Request(`https://api.twitch.tv/helix/videos?user_id=${ userId }`, twitchVideosInit);
+    return fetch(request).then(function(response) {
+        if(response.ok) {
+            return response.json()
+        }
+    })
+}
+window.getVideos = getVideos;
+
+export const getPubgVideos = videoId => {
+    const twitchPubgInit = {
+        method: 'get',
+        headers: {
+            "Accept": "application/vnd.twitchtv.v5+json",
+            'Client-Id': `${ Key.twitchAPI }`
+        }
+    }
+    let request = new Request(`https://api.twitch.tv/kraken/videos/${ videoId }`, twitchPubgInit);
+    return fetch(request)
+}
+
+
+export const timeGreaterThan = (t1, t2) => {
+    // debugger
+    let t3 = new Date(t1);
+    let t4 = new Date(t2);
+
+    if(t3 >= t4) {
+        return true
+    } else {
+        return false
+    }
+}
+
+export const timeGreaterThan2 = (t1, t2, seconds) => {
+    // debugger
+    let t3 = new Date(t1);
+    let t4 = new Date(t2);
+    t4.setHours(t4.getHours(), t4.getMinutes(), t4.getSeconds + seconds);
+    if (t3 <= t4) {
+        return true
+    } else {
+        return false
+    }
+    // let hours = t4.getHours();
+    // let minutes = t4.getMinutes();
+    // let secs = t4.getSeconds();
+    // if(seconds + sec < 60) {
+    //     t4.setHours(hours, minutes, seconds + secs)
+    // } else if(seconds + sec === 60) {
+    //     t4.setHours(hours, minutes + 1, 0)
+    // } else if(seconds + sec > 60) {
+    //     let newSecs = (seconds + sec) % 60;
+    //     let newMinutes = ((seconds + sec) - newSecs) / 60;
+    //     let min;
+    //     let hours;
+    //     if(newMinutes > 60) {
+    //         min = newMinutes % 60;
+    //         hours = (newMinutes - min) / 60;
+    //     }
+    // }
+}
+
+export const timestamp = (t1, t2, seconds) => {
+    // debugger
+    let t3 = new Date(t1);
+    let t4 = new Date(t2);
+    t4.setHours(t4.getHours(), t4.getMinutes(), t4.getSeconds + seconds);
+    return ((t4 - t3) / 1000)
+}
