adb7fc4092da8bf54981ac9f5bbf2824ce7d227a
swatjoker <iSWATxJOKERi>
2020-10-18 21:13:26 -0400

    app

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.listen(PORT, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("dist"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
