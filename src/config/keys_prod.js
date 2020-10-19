bbab614d033905534f80850445e9933d82638b84
swatjoker <iSWATxJOKERi>
2020-10-18 20:51:38 -0400

    keys fix

---
+++ src/config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pubgAPI: process.env.PUBG_API,
+    twitchAPI: process.env.TWITCH_API,
+    clientSECRET: process.env.CLIENT_SECRET,
+    oAUTH: process.env.O_AUTH,
+    gameID: process.env.GAME_ID
+}
